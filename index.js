7ad140b7176e574f19c73476a254b232fa558157
Rohit <ts.rohitsen@gmail.com>
2025-03-24 20:52:58 +0600

    Added Basic Rate Limiting

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ require("dotenv").config();
 const { setupBot } = require('./src/bot');
 const { camelotMonitor } = require('./src/dexMonitoring/camelotMonitor');
 const { uniswapMonitor } = require('./src/dexMonitoring/uniswapMonitor');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 50, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later."
+});
+
+app.use(limiter);
 
 app.get('/', (req, res)=>{
   res.send("Bot is live");
